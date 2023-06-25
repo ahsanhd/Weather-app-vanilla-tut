5cf60bc55eb8d3b6c553c19de8bfb8868adb4af2
ahsanhd <112783139+ahsanhd@users.noreply.github.com>
2023-06-25 17:25:44 +0500

    Feature: setting up api for weather data

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
- import './style.css'
+import "./style.css";
+import { getWeather } from "./weather";
+
+getWeather(10.12, 10.12, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+  (data) => {
+    console.log(data);
+  }
+);
