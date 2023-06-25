b36e6e90bb16995850d42dd0eedb0c867e11db5e
ahsanhd <112783139+ahsanhd@users.noreply.github.com>
2023-06-26 01:13:13 +0500

    Feature: Added Rendering Functions for interface

--- main.js
+++ main.js
@@ -1,8 +1,80 @@
 import "./style.css";
 import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMap";
 
-getWeather(10.12, 10.12, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-  (data) => {
-    console.log(data);
-  }
-);
+getWeather(45, 30, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("There was some issue getting weather details of your location");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
+  document.body.classList.remove("blurred");
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  return (parent.querySelector(`[data-${selector}]`).textContent = value);
+}
+
+const currentIcon = document.querySelector("[data-current-icon]");
+
+function getIconUrl(iconCode) {
+  return `./${ICON_MAP.get(iconCode)}.svg`;
+}
+function renderCurrentWeather(current) {
+  currentIcon.src = getIconUrl(current.iconCode);
+  console.log(current);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+
+  // Without setValue() helper function
+  // document.querySelector("[data-current-temp]").textContent =
+  //   current.currentTemp;
+  // document.querySelector("[data-current-high]").textContent = current.highTemp;
+}
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" });
+
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
+
+function renderDailyWeather(daily) {
+  // console.log({ daily });
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
+}
+
+const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" });
+
+const hourlySection = document.querySelector("[data-hour-section]");
+const hourRowTemplate = document.getElementById("hour-row-template");
+
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = "";
+  console.log({ hourly });
+  hourly.forEach((hour) => {
+    const element = hourRowTemplate.content.cloneNode(true);
+    setValue("temp", hour.maxTemp, { parent: element });
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element });
+    setValue("date", HOUR_FORMATTER.format(hour.timestamp), {
+      parent: element,
+    });
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    hourlySection.append(element);
+  });
+}
